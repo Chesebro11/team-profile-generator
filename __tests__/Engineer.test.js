0b46e3f9c5a68782c87787706a8506c710889fe4
unknown <chesebrojared@gmail.com>
2021-10-12 05:51:32 -0700

    first engineer test created

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@jest/globals')
+const Engineer = require('../lib/Engineer')
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer('Jared', 2, 'chesebrojared@gmail.com', 'chesebro11')
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+})
