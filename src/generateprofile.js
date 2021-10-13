2362bf2588fdccd2e9f9c13f0e4e1295f5efee59
unknown <chesebrojared@gmail.com>
2021-10-12 17:13:16 -0700

    created Engineer card function

--- src/generateprofile.js
+++ src/generateprofile.js
@@ -17,3 +17,24 @@ const generateManager = function (manager) {
     </div>
     `;
 }
+
+const generateEngineer = function (Engineer) {
+    return `
+    <div class='col-4 mt-4>
+        <div class='card h-50>
+            <div class='card-header'>
+                <h3>${Engineer.name}</h3>
+                <h4>Engineer</h4><i class='material-icons'></i>
+            </div>
+        
+        <div class='card-body'>
+            <p class='id'>ID: ${Engineer.id}</p>
+            <p class='email'>Email: <a href='mailto:${Engineer.email}'>${Engineer.email}</a></p>
+            <p class="github">Github: <a href="https://github.com/${Engineer.github}">${Engineer.github}</a></p>
+        </div>
+        
+        </div>
+    </div>
+`;
+
+}
